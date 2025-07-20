a7331d90faf98adeb982b91262849853f7619929
StevenAlfon <saayojoshua@gmail.com>
2025-07-20 12:17:33 +0800

    update

--- Reverse Fantasy/game/script/game.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var pause_menu: Control = $PauseMenu
-
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):  # "Esc" key by default
-		pause_menu.toggle_pause()
